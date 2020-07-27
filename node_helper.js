a941b3211e7dbffcc972e7721e0d66354a78e44d
Justin Reimer <jreimer15@my.whitworth.edu>
2020-07-27 09:46:40 -0700

    add node_helper

---
+++ node_helper.js
@@ -0,0 +1,36 @@
+const NodeHelper = require("node_helper");
+const ping = require("puppeteer");
+
+module.exports = NodeHelper.create({
+  start: function() {
+    console.log(this.name + " helper started ...");
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "MMM_TRACKING_GET_HTML_FOR_URL") {
+      const self = this;
+
+      puppeteer.launch()
+      .then(function(browser) {
+        return browser.newPage();
+      })
+      .then(function(page) {
+        return page.goto(payload.url).then(function() {
+          return page.content();
+        });
+      })
+      .then(function(html) {
+        self.sendSocketNotification(
+          "MMM_TRACKING_GET_HTML_FOR_URL_SUCCEEDED",
+          html,
+        );
+      })
+      .catch(function(err) {
+        self.sendSocketNotification(
+          "MMM_TRACKING_GET_HTML_FOR_URL_FAILED",
+          html,
+        );
+      });     
+    }
+  },
+});
