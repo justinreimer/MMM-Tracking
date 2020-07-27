cb293562818cc1bb92edcc03fd8aefe7e8c08b8b
Justin Reimer <jreimer15@my.whitworth.edu>
2020-07-27 15:15:39 -0700

    try out ups

--- MMM-Tracking.js
+++ MMM-Tracking.js
@@ -13,31 +13,12 @@ Module.register("MMM-Tracking", {
 
   // Define required scripts.
   getStyles: function() {
-		return [ "MMM-Tracking.css" ]
-	},
+    return [ "MMM-Tracking.css" ];
+  },
 
   // Define start sequence.
   start: function () {
     Log.info("Starting module: " + this.name);
-
-    //TODO: delete this if not needed
-    //uncomment if Object.keys is not defined
-    /*
-    if (!Object.keys) {
-      Object.keys = function (obj) {
-        var keys = [];
-        var k;
-
-        for (k in obj) {
-          if (Object.prototype.hasOwnProperty.call(obj, k)) {
-            keys.push(k);
-          }
-        }
-
-        return keys;
-      };
-    }
-    */
 
     this.trackingResults = {
       fedex: {},
@@ -52,6 +33,26 @@ Module.register("MMM-Tracking", {
     };
 
     this.scheduleUpdate(this.config.initialLoadDelay);
+  },
+
+  // Handle socket answer
+  socketNotificationReceived: function(notification, payload) {
+    // Care only own socket answers
+    if (notification === "MMM_TRACKING_GET_HTML_FOR_URL_FAILED") {
+      Log.error("could not fetch html for carrier: " + payload.carrier);
+      this.trackingResults[payload.carrier]["Error: "] = "Could not get html for scraping.";
+    } else if (notification === "MMM_TRACKING_GET_HTML_FOR_URL_SUCCEEDED") {
+      switch(payload.carrier) {
+        case "ups":
+          procesUpsTrackingHtml(payload.html);
+          brea;
+        case "usps"
+          procesUspsTrackingHtml(payload.html);
+          break;
+        default:
+          break;
+      }
+    }
   },
 
   resetTrackingResults: function() {
@@ -99,6 +100,40 @@ Module.register("MMM-Tracking", {
         return i + "rd";
     }
     return i + "th";
+  },
+
+  dayLookup: {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "saturday"
+  },
+
+  monthLookup: {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July"
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December"
+  },
+
+  getDeliveryStringFromDate: function(date) {
+    var weekday = this.dayLookup[date.getDay()];
+    var monthString = this.monthLookup[date.getMonth()];
+    var dayNumber = this.getOrdinalSuffix(date.getDate());
+    var year = date.getFullYear();
+
+    return weekday + " " + monthString + " " + dayNumber + ", " + year;
   },
   
   getTableLine: function(table, cell1, cell2) {
@@ -245,7 +280,7 @@ Module.register("MMM-Tracking", {
   },
 
   processTrackingNumbers: function (data) {
-    this.processUpsTrackingNumbers(data.ups, this.carrierProcessingFinishedCallback.bind(this));
+    this.processUpsTrackingNumbers(data.ups);
     this.processUspsTrackingNumbers(data.usps, this.carrierProcessingFinishedCallback.bind(this));
     this.processFedexTrackingNumbers(data.fedex, this.carrierProcessingFinishedCallback.bind(this));
   },
@@ -319,33 +354,35 @@ Module.register("MMM-Tracking", {
     fedexTrackingRequest.send();
   },
 
-  procesUSPSTrackingHtml: function(responseText) {
+  procesUspsTrackingHtml: function(responseText) {
     parser=new DOMParser();
     var dom = parser.parseFromString(responseText,"text/html");
     
-    trackingResults = dom.getElementsByClassName('track-bar-container');
+    trackingResults = dom.getElementsByClassName("track-bar-container");
 
     var self = this;
 
-    Array.from(trackingResults).forEach(function(node) {
+    Array.from(trackingResults).forEach(function(node, i) {
 
+      var trackingNumber;
       try{
-        var trackingNumber = node.getElementsByClassName('tracking-number')[0].textContent.trim();
+        trackingNumber = node.getElementsByClassName("tracking-number")[0].textContent.trim();
       } catch(e) {
-        Log.error("DOM structure for usps tracking numbers has changed. Could not obtain tracing number.");
+        trackingNumber = "Unknown" + i;
+        Log.error("DOM structure for usps tracking numbers has changed. Could not obtain tracking number.");
       }
 
-      if(node.getElementsByClassName('delivery_delivered').length) {
+      if(node.getElementsByClassName("delivery_delivered").length) {
         self.trackingResults.usps[trackingNumber] = "Delivered";
         return;
       }
 
       try{
-        var weekday = node.getElementsByClassName('day')[0].textContent;
-        var dayNumber = node.getElementsByClassName('date')[0].textContent;
+        var weekday = node.getElementsByClassName("day")[0].textContent;
+        var dayNumber = node.getElementsByClassName("date")[0].textContent;
         dayNumber = self.getOrdinalSuffix(dayNumber);
 
-        var monthYearNode = node.getElementsByClassName('month_year')[0];
+        var monthYearNode = node.getElementsByClassName("month_year")[0];
         var monthNode = monthYearNode.getElementsByTagName("span")[0];
 
         var monthString = monthNode.textContent.trim();
@@ -362,7 +399,8 @@ Module.register("MMM-Tracking", {
 
         self.trackingResults.usps[trackingNumber] = deliveryEstimateString;
       } catch(e) {
-        Log.error("could not determine deliver date for usps tracking number " + trackingNumber + ". This is probably because of an an unexpected DOM format.")
+        self.trackingResults.usps[trackingNumber] = "Unexpected Dom Format";
+        Log.error("could not determine deliver date for usps tracking number " + trackingNumber + ". This is probably because of an an unexpected DOM format for usps numbers.")
       }
     });
   },
@@ -389,7 +427,7 @@ Module.register("MMM-Tracking", {
     uspsTrackingRequest.onreadystatechange = function () {
       if (this.readyState === 4) {
         if (this.status === 200) {
-          self.procesUSPSTrackingHtml(this.responseText);
+          self.procesUspsTrackingHtml(this.responseText);
           self.trackingSourcesStatus.usps = "succeeded";
         } else {
           self.trackingSourcesStatus.usps = "failed";
@@ -407,7 +445,68 @@ Module.register("MMM-Tracking", {
     uspsTrackingRequest.send();
   },
 
-  processUpsTrackingNumbers: function(trackingNumbers, callback) {
+  processUpsTrackingHtml: function(responseText, callback) {
+    if(this.trackingNumbers.ups.length > 1) {
+      processMultipleUpsNumbersHtml(responseText);
+    } else {
+      processSingleUpsNumberHtml(responseText);
+    }
+
+    this.carrierProcessingFinishedCallback()
+  },
+
+  processMultipleUpsNumbersHtml: function(responseText) {
+    parser=new DOMParser();
+    var dom = parser.parseFromString(responseText,"text/html");
+    
+    trackingResults = dom.querySelectorAll( "li div.ups-card" );
+
+    var self = this;
+
+    Array.from(trackingResults).forEach(function(node, i) {
+
+      var trackingNumber;
+      try{
+        trackingNumber = node.querySelector("a").textContent.trim()
+      } catch(e) {
+        trackingNumber = "Unknown" + i;
+        Log.error("DOM structure for multiple ups tracking numbers has changed. Could not obtain tracking number.");
+      }
+
+      if(node.querySelector("#stApp_SummaryTracked_packageStatusDesciption_0").textContent.trim().toLowerCase === "delivered") {
+        self.trackingResults.usps[trackingNumber] = "Delivered";
+        return;
+      }
+
+      try{
+        var dateNode = node.querySelectorAll(".row")[1].querySelector("div");
+        var statusNode = dateNode.querySelector("strong");
+        dateNode.removeChild(statusNode);
+
+        var dateString = dateNode.textContent.trim();
+        
+        var now = new Date();
+
+        var currentYear = new now.getFullYear();
+
+        var deliveryDate = new Date(currentYear + "/" + dateString);
+
+        //package will be delivered after December 31st, so we need to increment the date.
+        if(deliveryDate < now) {
+          deliveryDate = new Date((currentYear + 1) + "/" + dateString);
+        }
+
+        var deliveryEstimateString = self.getDeliveryStringFromDate(deliverDate);
+
+        self.trackingResults.ups[trackingNumber] = deliveryEstimateString;
+      } catch(e) {
+        self.trackingResults.ups[trackingNumber] = "Unexpected Dom Format";
+        Log.error("could not determine deliver date for usps tracking number " + trackingNumber + ". This is probably because of an an unexpected DOM format for mulitple ups numbers.")
+      }
+    });
+  },
+
+  processUpsTrackingNumbers: function(trackingNumbers) {
     if (trackingNumbers.length === 0) {
       this.trackingSourcesStatus.ups = "succeeded";
       this.trackingResults.ups = {};
@@ -426,13 +525,13 @@ Module.register("MMM-Tracking", {
 
     url += "&requester=WT/tracksummary";
 
-    this.sendSocketNotification("MMM_NETWORKSIGNAL_CHECK_SIGNAL", {
+    this.sendSocketNotification("MMM_TRACKING_GET_HTML_FOR_URL", {
         {
           url: "",
           carrier: "ups"
         }
     });
-  }
+  },
 
   /* scheduleUpdate()
    * Schedule next update.
